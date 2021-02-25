dac1c507333afd1fac1c91058839c2239adee13a
kaan <kaansnl@icloud.com>
2021-02-25 19:37:45 +0300

    tiny tiny fix

--- src/Player.hpp
+++ src/Player.hpp
@@ -3,7 +3,7 @@
 
 class Player : public Entity{
 public:
-  Player(char* texturesheet, SDL_Rect dstrect, int actual_width, int actual_height, int sprite_amount, int movement_speed);
+  Player(char* texturesheet, SDL_Rect dstrect, int actual_width, int actual_height, int sprite_amount, int movement_speed, short damage);
   ~Player();
   void HandleMovement(const Uint8* state);
 };
