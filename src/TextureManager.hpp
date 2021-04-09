8533140c9f886b192e453f55c437b094cac0fa51
kaan <kaansnl@icloud.com>
2021-04-09 22:41:02 +0300

    add more error logging

--- src/TextureManager.hpp
+++ src/TextureManager.hpp
@@ -4,6 +4,7 @@
 #include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_ttf.h>
 #include <iostream>
+#include "SDL_error.h"
 #include "SceneManager.hpp"
 
 /* 
@@ -13,10 +14,14 @@ ALL OF THE FUNCTIONS ASSUMES THAT SDL_Init(), TTF_Init() and IMG_Init were calle
 class TextureManager{
 public:
     static inline SDL_Texture* load_image(const char* file){
-        SDL_Surface* temp_surface = IMG_Load(file);      
+        SDL_Surface* temp_surface = IMG_Load(file);
+        if(!temp_surface){
+            std::cout << "IMG_Load(" << file << ") call has returned nullptr. Error Code: "  << IMG_GetError() << std::endl;
+         }      
         SDL_Texture* texture = SDL_CreateTextureFromSurface(SceneManager::renderer, temp_surface);
         if (!texture){
             std::cout << "Texture loaded by TextureManager::load_image() may be invalid, image file name: " << file << std::endl;
+            std::cout << SDL_GetError() << std::endl;
         }
         SDL_FreeSurface(temp_surface);
         return texture;
@@ -26,12 +31,16 @@ public:
         TTF_Font* loaded_font = TTF_OpenFont(font_file, ptsize);
         if(!loaded_font){
             std::cout << "Failed loading font by TextureManager::load_ttf_font() , font file name: " << font_file << std::endl;
-            std::cout << "Tip: do TTF_Init() somewhere" << std::endl;
+            std::cout << "Error Code: " << TTF_GetError() << std::endl;
         }
         SDL_Surface* temp_surface = TTF_RenderText_Solid(loaded_font, text, color);
+        if(!temp_surface){
+            std::cout << "TTF_RenderText_Solid() failed, Error Code: " << SDL_GetError() << std::endl;
+        }
         SDL_Texture* texture = SDL_CreateTextureFromSurface(SceneManager::renderer, temp_surface);        
         if (!texture){
             std::cout << "Texture generation failed at TextureManager::load_ttf_font() " << font_file << std::endl;
+            std::cout << "Error Code: " << SDL_GetError() << std::endl;
         }
         SDL_FreeSurface(temp_surface);
         TTF_CloseFont(loaded_font);
@@ -50,7 +59,7 @@ public:
     static inline void render(SDL_Texture* texture, SDL_Rect* source, SDL_Rect* dest){
         if(check_bounds(dest)){ //check if destination rect is in the screen
             if(SDL_RenderCopy(SceneManager::renderer, texture, source, dest) != 0){
-                //std::cout << SDL_GetError() << " at x:" <<  dest->x << ", y: "  << dest->y << std::endl;
+                std::cout << "Failed rendering!!: " << SDL_GetError() << " at x:" <<  dest->x << ", y: "  << dest->y << std::endl;
               //  std::cout << "Texture: " << texture << std::endl;
             }
         }
@@ -60,10 +69,14 @@ public:
         if(check_bounds(dest)){
             SDL_SetRenderDrawColor(SceneManager::renderer, color.r, color.g, color.b, color.a);
             if(filled){
-                SDL_RenderFillRect(SceneManager::renderer, dest);
+                if(SDL_RenderFillRect(SceneManager::renderer, dest) != 0){
+                    std::cout << "SDL_RenderFillRect() failed! Error Code: " << SDL_GetError() << std::endl;
+                };
             }
             else{
-                SDL_RenderDrawRect(SceneManager::renderer, dest);
+                if(SDL_RenderDrawRect(SceneManager::renderer, dest) != 0){
+                    std::cout << "SDL_DrawRect() failed! Error Code: " << SDL_GetError() << std::endl;
+                }
             }
             SDL_SetRenderDrawColor(SceneManager::renderer, 0, 0, 0, 0);
         }
